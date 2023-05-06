c39b41b4add588e04f15ed9a061e8fe07fc4d254
jaffrantirta <jaffrantirta@icloud.com>
2023-05-06 11:44:45 +0800

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CashController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\JournalController;
 use App\Http\Controllers\TransactionController;
 use App\Models\Transaction;
@@ -32,9 +33,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard', ['roles' => session('user_roles')]);
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::middleware(['permission:create-account', 'permission:update-account', 'permission:delete-account', 'permission:view-account'])->group(function () {
@@ -43,26 +42,12 @@ Route::middleware('auth')->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    Route::get('/accounts', [AccountController::class, 'index'])->name('account.index');
-    Route::patch('/accounts', [AccountController::class, 'update'])->name('account.update');
-    Route::post('/accounts', [AccountController::class, 'store'])->name('account.store');
-    Route::delete('/accounts/{account}', [AccountController::class, 'destroy'])->name('account.destroy');
-
-    // Route::get('/items', [ItemController::class, 'index'])->name('item.index');
-    // Route::patch('/items', [ItemController::class, 'update'])->name('item.update');
-    // Route::post('/items', [ItemController::class, 'store'])->name('item.store');
-    // Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('item.destroy');
-
-    Route::get('/journals', [JournalController::class, 'index'])->name('journal.index');
-    Route::get('/journals/create', [JournalController::class, 'create'])->name('journal.create');
-    Route::patch('/journals', [JournalController::class, 'update'])->name('journal.update');
-    Route::post('/journals', [JournalController::class, 'store'])->name('journal.store');
-    Route::delete('/journals/{journal}', [JournalController::class, 'destroy'])->name('journal.destroy');
-
     Route::resource('cash', CashController::class);
     Route::resource('transaction', TransactionController::class);
     Route::resource('category', CategoryController::class);
     Route::resource('item', ItemController::class);
+    Route::resource('journal', JournalController::class);
+    Route::resource('account', AccountController::class);
 });
 
 require __DIR__ . '/auth.php';
