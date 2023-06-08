566ae7909a88fe7cb1e8a1dd857af771ec0c2e50
jaffrantirta <jaffrantirta@icloud.com>
2023-06-08 10:35:16 +0800

    view login

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Auth/Login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
