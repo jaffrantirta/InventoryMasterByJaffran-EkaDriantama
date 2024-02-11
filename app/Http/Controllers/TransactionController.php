9c854d8158501b2b059f80b758cb3789fc76534f
jaffrantirta <jaffrantirta@icloud.com>
2024-02-11 22:40:28 +0800

    mantap

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,66 +4,67 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TransactionStorePurchaseRequest;
 use App\Http\Requests\TransactionStoreRequest;
-use App\Models\AccountSetting;
 use App\Models\Account;
+use App\Models\AccountSetting;
 use App\Models\Cash;
 use App\Models\Item;
 use App\Models\Journal;
 use App\Models\JournalDetail;
 use App\Models\Purchase;
+use App\Models\StockHistory;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
-use App\Models\StockHistory;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
 
 class TransactionController extends Controller
 {
     public function index(Request $request)
-{
-    $search = $request->query('search');
-    $page = $request->query('page') ?? 1;
+    {
+        $search = $request->query('search');
+        $page = $request->query('page') ?? 1;
 
-    $query = Transaction::doesntHave('purchase')->latest();
+        $query = Transaction::doesntHave('purchase')->latest();
 
-    if ($search) {
-        $query->where(function ($query) use ($search) {
-            $query->where('reference_code', 'like', '%' . $search . '%');
-        });
+        if ($search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('reference_code', 'like', '%' . $search . '%');
+            });
+        }
+
+        $transactions = $query->paginate(5, ['*'], 'page', $page);
+
+        return Inertia::render('Transaction/Show', [
+            'roles' => session('user_roles'),
+            'transactions' => $transactions,
+            'search' => $search  // Pass the search query to the view
+        ]);
     }
-
-    $transactions = $query->paginate(5, ['*'], 'page', $page);
-
-    return Inertia::render('Transaction/Show', [
-        'roles' => session('user_roles'),
-        'transactions' => $transactions,
-        'search' => $search // Pass the search query to the view
-    ]);
-}
 
     public function indexPurchase(Request $request)
     {
         $search = $request->query('search');
-    $page = $request->query('page') ?? 1;
+        $page = $request->query('page') ?? 1;
 
-    $query = Transaction::whereHas('purchase')->latest();
+        $query = Transaction::whereHas('purchase')->latest();
 
-    if ($search) {
-        $query->where(function ($query) use ($search) {
-            $query->where('reference_code', 'like', '%' . $search . '%');
-        });
+        if ($search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('reference_code', 'like', '%' . $search . '%');
+            });
+        }
+
+        $transactions = $query->paginate(5, ['*'], 'page', $page);
+
+        return Inertia::render('Transaction/ShowPurchase', [
+            'roles' => session('user_roles'),
+            'transactions' => $transactions,
+            'search' => $search  // Pass the search query to the view
+        ]);
     }
 
-    $transactions = $query->paginate(5, ['*'], 'page', $page);
-
-    return Inertia::render('Transaction/ShowPurchase', [
-        'roles' => session('user_roles'),
-        'transactions' => $transactions,
-        'search' => $search // Pass the search query to the view
-    ]);
-    }
     public function create()
     {
         return Inertia::render('Transaction/Create', [
@@ -72,6 +73,7 @@ class TransactionController extends Controller
             'reference_code' => now()->timestamp,
         ]);
     }
+
     public function createPurchase()
     {
         return Inertia::render('Transaction/CreatePurchase', [
@@ -80,12 +82,13 @@ class TransactionController extends Controller
             'reference_code' => now()->timestamp,
         ]);
     }
+
     public function store(TransactionStoreRequest $request)
     {
         // dd($request);
         DB::beginTransaction();
 
-        //get setting
+        // get setting
         $cash_account = AccountSetting::where('name', 'kas')->first();
         $income_account = AccountSetting::where('name', 'pendapatan-usaha')->first();
 
@@ -95,21 +98,22 @@ class TransactionController extends Controller
             $grand_total += $item['price'] * $item['qty'];
         }
 
-        if($request->input('discount'))$grand_total = $grand_total - $request->input('discount');
+        if ($request->input('discount'))
+            $grand_total = $grand_total - $request->input('discount');
 
-        //insert journal
+        // insert journal
         $journalData = $request->only(['date']);
         $journalData['user_id'] = auth()->user()->id;
         $journalData['description'] = 'Penjualan dengan kode transaksi : ' . $request->input('reference_code');
         $journal = Journal::create($journalData);
 
-        //update balance
+        // update balance
         $cash = Account::find($cash_account->account_id);
         $income = Account::find($income_account->account_id);
         $cash->update(['initial_balance' => $cash->initial_balance + $grand_total]);
         $income->update(['initial_balance' => $income->initial_balance + $grand_total]);
 
-        //insert journal detail
+        // insert journal detail
         JournalDetail::create([
             'journal_id' => $journal->id,
             'account_id' => $cash_account->account_id,
@@ -123,14 +127,14 @@ class TransactionController extends Controller
             'credit' => $grand_total,
         ]);
 
-        //insert cash
+        // insert cash
         $cash = Cash::create([
             'journal_id' => $journal->id,
             'type' => 'in',
             'user_id' => auth()->user()->id,
         ]);
 
-        //insert transaction
+        // insert transaction
         $transaction = Transaction::create([
             'reference_code' => $request->input('reference_code'),
             'user_id' => auth()->user()->id,
@@ -138,7 +142,7 @@ class TransactionController extends Controller
             'grand_total' => $grand_total
         ]);
 
-        //insert transaction detail
+        // insert transaction detail
         foreach ($request->input('items_selected') as $detail) {
             $item = Item::with('unit')->find($detail['item_id']);
             $final_qty = 0;
@@ -153,7 +157,8 @@ class TransactionController extends Controller
                 $price = $item->price;
                 $total = $detail['qty'] * $price;
             }
-            if ($final_qty > $item->stock) return redirect()->back()->withErrors(['Stok kurang']);
+            if ($final_qty > $item->stock)
+                return redirect()->back()->withErrors(['Stok kurang']);
             $stock = $item->stock - $final_qty;
             $item->stock = $stock;
             $item->save();
@@ -175,12 +180,13 @@ class TransactionController extends Controller
 
         return Redirect::route('transaction.index');
     }
+
     public function storePurchase(TransactionStorePurchaseRequest $request)
     {
         // dd($request);
         DB::beginTransaction();
 
-        //get setting
+        // get setting
         $cash_account = AccountSetting::where('name', 'kas')->first();
         $income_account = AccountSetting::where('name', 'pembelian')->first();
 
@@ -190,20 +196,20 @@ class TransactionController extends Controller
             $grand_total += $item['price'] * $item['qty'];
         }
 
-        //insert journal
+        // insert journal
         $journalData = $request->only(['date']);
         $journalData['user_id'] = auth()->user()->id;
         $journalData['description'] = 'Pembelian dengan kode transaksi : ' . $request->input('reference_code');
         $journal = Journal::create($journalData);
 
-        //update balance
+        // update balance
         $cash = Account::find($cash_account->account_id);
         $income = Account::find($income_account->account_id);
         $cash->update(['initial_balance' => $cash->initial_balance - $grand_total]);
         $income->update(['initial_balance' => $income->initial_balance - $grand_total]);
 
-        //insert journal detail
-        
+        // insert journal detail
+
         JournalDetail::create([
             'journal_id' => $journal->id,
             'account_id' => $income_account->account_id,
@@ -218,14 +224,14 @@ class TransactionController extends Controller
             'credit' => $grand_total,
         ]);
 
-        //insert cash
+        // insert cash
         $cash = Cash::create([
             'journal_id' => $journal->id,
             'type' => 'in',
             'user_id' => auth()->user()->id,
         ]);
 
-        //insert transaction
+        // insert transaction
         $transaction = Transaction::create([
             'reference_code' => $request->input('reference_code'),
             'user_id' => auth()->user()->id,
@@ -233,12 +239,12 @@ class TransactionController extends Controller
             'grand_total' => $grand_total
         ]);
 
-        //insert transaction purchase
+        // insert transaction purchase
         Purchase::create([
             'transaction_id' => $transaction->id,
         ]);
 
-        //insert transaction detail
+        // insert transaction detail
         foreach ($request->input('items_selected') as $detail) {
             $item = Item::with('unit')->find($detail['item_id']);
             $final_qty = 0;
@@ -274,6 +280,7 @@ class TransactionController extends Controller
 
         return Redirect::route('transaction.index.purchase');
     }
+
     public function showDetail(string $id)
     {
         return Inertia::render('Transaction/Detail', [
@@ -281,6 +288,7 @@ class TransactionController extends Controller
             'transaction' => Transaction::with('transaction_details.item')->where('id', $id)->first(),
         ]);
     }
+
     public function destroy(Transaction $transaction)
     {
         //
